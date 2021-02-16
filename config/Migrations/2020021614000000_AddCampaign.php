7c4bdb073bd497ae690283dcff363debd4e82b10
Massimo Infunti <massimoi@nkoni.org>
2021-02-16 08:49:58 +0100

    adding campaign_id to the mail queue and corresponding method

---
+++ config/Migrations/2020021614000000_AddCampaign.php
@@ -0,0 +1,37 @@
+<?php
+
+use Migrations\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class AlterTemplateToEmailQueue extends AbstractMigration
+{
+  /**
+   * Up Method
+   *
+   * More information on this method is available here:
+   * http://docs.phinx.org/en/latest/migrations.html#the-up-method
+   *
+   * @return void
+   */
+  public function up()
+  {
+    $this->table('email_queue')
+      ->addColumn('campaign_id', 'int')
+      ->update();
+  }
+
+  /**
+   * Down Method
+   *
+   * More information on this method is available here:
+   * http://docs.phinx.org/en/latest/migrations.html#the-down-method
+   *
+   * @return void
+   */
+  public function down()
+  {
+    $this->table('email_queue')
+      ->removeColumn('campaign_id', 'int')
+      ->update();
+  }
+}
